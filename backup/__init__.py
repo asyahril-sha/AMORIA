231f9449fd8f482021523ad477bc4843ec0d16f2
asyahril-sha <asyahril.aiart@gmail.com>
2026-03-24 07:27:25 +0800

    Update __init__.py

--- backup/__init__.py
+++ backup/__init__.py
@@ -1,1 +1,20 @@
+# backup/__init__.py
+# -*- coding: utf-8 -*-
+"""
+=============================================================================
+AMORIA - Virtual Human dengan Jiwa
+Backup Package
+=============================================================================
+"""
+
+from .automated import AutoBackup, get_backup_manager
+from .recovery import RecoveryManager
+from .verify import BackupVerifier
+
+__all__ = [
+    'AutoBackup',
+    'get_backup_manager',
+    'RecoveryManager',
+    'BackupVerifier',
+]
 
